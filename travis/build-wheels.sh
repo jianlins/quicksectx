3dee0f32c3f111540c32fd1534a53fc76b73c257
jianlins <jianlinshi.cn@gmail.com>
2019-12-29 14:46:02 -0700

    try manylinux demo

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -4,11 +4,13 @@ set -e -x
 # Install a system package required by our library
 yum install -y atlas-devel
 
+PYS=(/opt/python/cp36-cp36m/bin, /opt/python/cp37-cp37m/bin)
 # Compile wheels
-for PYBIN in /opt/python/*/bin; do
+for PYBIN in PYS; do
     "${PYBIN}/pip" install -r /io/dev-requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
+
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
@@ -16,7 +18,7 @@ for whl in wheelhouse/*.whl; do
 done
 
 # Install packages and test
-for PYBIN in /opt/python/*/bin/; do
+for PYBIN in PYS; do
     "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
     (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
 done
