e02b0764d2c8d5dd47473db5766b96d09b7d9509
jianlins <jianlinshi.cn@gmail.com>
2019-12-28 20:21:28 -0700

    fix compile

--- quicksectx/__init__.py
+++ quicksectx/__init__.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from .quicksect1 import Interval, IntervalNode, IntervalTree, distance
 from .quicksect2 import IntervalX, IntervalNodeX, IntervalTreeX, distancex
 # from quicksectx.src import quicksect1 as quicksect
--- quicksectx/version.py
+++ quicksectx/version.py
@@ -1,3 +1,3 @@
-__version__ = '0.2.2a4'
+__version__ = '0.2.2a5'
 if __name__ == '__main__':
     print(__version__)
