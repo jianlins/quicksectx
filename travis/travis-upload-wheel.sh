7023393fb211752d709c64c371891c19753d4bfb
jianlins <jianlinshi.cn@gmail.com>
2019-12-29 14:31:16 -0700

    try manylinux demo

---
+++ travis/travis-upload-wheel.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+set -ex
+
+export CLOUD_CONTAINER_NAME=travis-dev-wheels
+
+if [[ ( ${USE_WHEEL} == 1 ) \
+      && ( "${TRAVIS_BRANCH}" == "master" ) \
+      && ( "${TRAVIS_PULL_REQUEST}" == "false" ) ]]; then
+  /opt/python/${PY36}/bin/pip install wheelhouse_uploader
+  /opt/python/${PY36}/bin/python -m wheelhouse_uploader upload --local-folder \
+    ${TRAVIS_BUILD_DIR}/dist/ ${CLOUD_CONTAINER_NAME}
+fi
