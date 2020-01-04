45976635e028b9111adae73c2480baf9f40e91b9
jianlins <jianlinshi.cn@gmail.com>
2020-01-03 23:52:24 -0700

    0.2.9a2

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -3,21 +3,34 @@ set -e -x
 
 # Install a system package required by our library
 yum install -y atlas-devel
-PYBIN=/opt/python/$1/bin
-PLAT=$2
+ARGS=("$@")
+
+PLAT=${ARGS[0]}
+PROJECT_NAME=${ARGS[1]}
+PYBINS=${ARGS[@]:2}
 # Compile wheels
-"${PYBIN}/pip" install -r /io/dev-requirements.txt
-"${PYBIN}/pip" wheel /io/ -w wheelhouse/
-
-
+echo "${PYBINS[@]}"
+for PYBIN in ${PYBINS[@]};do
+  PYBIN="/opt/python/${PYBIN}/bin"
+  echo "${PYBIN}"
+  "${PYBIN}/pip" install -q -r /io/dev-requirements.txt
+  "${PYBIN}/python" -m spacy download en
+  "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
-    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+    if [[ $whl == wheelhouse/${PROJECT_NAME}* ]]; then
+      auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+    else
+      rm $whl
+    fi
 done
 
+ls /io/wheelhouse -l
 # Install packages and test
-for PYBIN in ${PYS}; do
-    "${PYBIN}/pip" install quicksectx --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/nosetests" quicksectx)
+for PYBIN in ${PYBINS[@]}; do
+    PYBIN="/opt/python/${PYBIN}/bin"
+    "${PYBIN}/pip" install ${PROJECT_NAME} --no-index -f /io/wheelhouse
+    (cd "$HOME";ls -l; "${PYBIN}/nosetests" ${PROJECT_NAME})
 done
