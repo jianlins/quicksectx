edde8483062d5d14ba106dfacef74b3ff272acbe
Jianlin Shi <jianlins@users.noreply.github.com>
2023-05-29 15:49:12 -0600

    test

--- quicksectx/version.py
+++ quicksectx/version.py
@@ -1,5 +1,3 @@
-import os
-# use this to satisfy toml dynamic version configure without import othere dependencies
-__version__ = open(os.path.join(os.path.dirname(__file__), 'VERSION')).read()
+__version__ = "0.3.5"
 if __name__ == '__main__':
     print(__version__)
