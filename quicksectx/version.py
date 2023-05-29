8d87dadd9ceb97b16f641eea8a9992fe7dd1a94b
Jianlin Shi <jianlins@users.noreply.github.com>
2023-05-29 14:36:03 -0600

    update toml config

--- quicksectx/version.py
+++ quicksectx/version.py
@@ -1,3 +1,5 @@
-__version__ = '0.3.5'
+import os
+# use this to satisfy toml dynamic version configure without import othere dependencies
+__version__ = open(os.path.join(os.path.dirname(__file__), 'VERSION')).read()
 if __name__ == '__main__':
     print(__version__)
